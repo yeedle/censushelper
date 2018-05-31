01b8b20cca7b2b43338f7bb324c73b27208fbf57
yeedle <abeneuwirth@gmail.com>
2018-05-31 11:33:32 -0400

    stages package framework

---
+++ R/utils.R
@@ -0,0 +1,33 @@
+
+#' map it.
+#'
+#' maps a census variable.
+#'
+#' variables need to be renamed to value instead of estimate.
+#'
+#' @param sf An object of class `sf`
+#'
+#' @export
+#'
+#' @examples
+map_it <- function(sf, palette = "OrRd", provider = leaflet::providers$CartoDB.Positron, ...) {
+  pal <- leaflet::colorNumeric(palette = palette, domain = sf %>% dplyr::pull(value))
+  leaflet::leaflet(sf) %>%
+    leaflet::addProviderTiles(provider) %>%
+    leaflet::addPolygons(
+      fillColor = ~pal(value),
+      weight = .1,
+      opacity = .5,
+      fillOpacity = 0.7,
+      stroke = F,
+      label = as.character(sf$value),
+      highlight = leaflet::highlightOptions(
+        weight = 5,
+        stroke = T,
+        color = "#666",
+        fillOpacity = 0.7,
+        bringToFront = TRUE
+        ),
+      ...
+      )
+}
